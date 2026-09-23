30ce5eab961a4ad5d3f63bd4a53a8a4f48564a95
jbinene0 <jbinene0@users.noreply.github.com>
2026-09-23 13:29:46 -0600

    worked on routing

--- server.js
+++ server.js
@@ -1,16 +1,47 @@
-import express from 'express';
-
-const app = express();
+// Imports
+import express from 'express'
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const name = process.env.NAME; // <-- NEW
 
+/**
+ * Declare Important Variables
+ */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 3000;
+
+/**
+ * Setup Express Server
+ */
+const app = express();
+
+/**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
-    res.send(`Welcome, ${name}!`); // <-- UPDATED
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
-const PORT = 3000;
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+});
+
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+});
+
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
-    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+    console.log(`Server is running on http://127.0.0.1:${PORT}`)
 });
 
 app.get('/new-route', (req, res) => {
